e3b18840dfc2d52ef5b283eaf1266f854a4222a5
Prasadchaskar <chaskarprasad2000.com>
2021-09-27 15:35:04 +0530

    Update Form Added

--- InterviewDash/settings.py
+++ InterviewDash/settings.py
@@ -129,6 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
